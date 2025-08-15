831bc94253dbc2c914fc554b8439ba3c5beec8d5
DuncanLin <ken21181238@gmail.com>
2025-08-15 22:16:17 +0800

    優化bg-header

--- src/components/bg-header.vue
+++ src/components/bg-header.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="d-flex">
-    <div class="bg-header rounded-bottom-pill w-25 bg-primary-200"></div>
-    <div class="bg-header rounded-bottom-pill w-25 bg-primary-300"></div>
-    <div class="bg-header rounded-bottom-pill w-25 bg-primary-200"></div>
-    <div class="bg-header rounded-bottom-pill w-25 bg-primary-300"></div>
+  <div class="d-flex gap-0 bg-header">
+    <div class="w-25 bg-primary-200"></div>
+    <div class="w-25 bg-primary-300"></div>
+    <div class="w-25 bg-primary-200"></div>
+    <div class="w-25 bg-primary-300"></div>
+  </div>
+  <div class="u-header">
+    <div class="u-pill u-c1"></div>
+    <div class="u-pill u-c2"></div>
+    <div class="u-pill u-c1"></div>
+    <div class="u-pill u-c2"></div>
   </div>
 </template>
+
 <script setup></script>
