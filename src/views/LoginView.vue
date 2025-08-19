f7af61b26b3abaf451d108190c35acb8649fcef3
DuncanLin <ken21181238@gmail.com>
2025-08-20 01:31:26 +0800

    Add Login Page, Register Page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,1 +1,27 @@
-<template>123</template>
+<template>
+  <div class="loginBG">
+    <Header />
+    <div class="container">
+      <div class="">
+        <div class="w-50 mx-auto">
+          <div class="rounded-pill mili-img shadow-light mt-spac-5xl overflow-hidden">
+            <img src="../assets/images/login/milli.png" alt="image" class="object-fit-cover" />
+          </div>
+
+          <div class="mt-spac-m">
+            <img src="../assets/images/login/logo.svg" alt="logo" class="d-block w-100 h-auto" />
+          </div>
+          <h3 class="fs-5 text-center text-secondary-500 mb-0 mt-spac-m">聊出你的食物默契 ✨</h3>
+        </div>
+
+        <div class="login">
+          <button type="button" class="btn btn-primary w-100">登入</button>
+          <router-link to="/register" class="btn btn-secondary w-100">註冊</router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import Header from '../components/bg-header.vue'
+</script>
