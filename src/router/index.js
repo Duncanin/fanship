2c97a6f6aebb804121c1c644d8f7344259878ced
jhoujhou <a01450887@gmail.com>
2025-08-26 21:03:58 +0800

    feat: 更新路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
-// import TestView from '../views/TestView.vue'
+import HomeView from '../views/HomeView.vue'
+import TestView from '../views/TestView.vue'
 import PairingIndex from '../views/PairingIndex.vue'
 import PairingIcon from '../views/PairingIcon.vue'
 import PairingFind from '../views/PairingFind.vue'
@@ -11,16 +11,16 @@ import PairingGame from '../views/PairingGame.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView,
-    // },
-    // {
-    //   path: '/test',
-    //   name: 'test',
-    //   component: TestView,
-    // },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: TestView,
+    },
     {
       path: '/pairing-index',
       name: 'pairingIndex',
