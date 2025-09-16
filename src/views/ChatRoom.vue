3f2e881c6d3fcb166ca028969c1cae69c95dec43
wateruby <wateruby8@gmail.com>
2025-09-16 23:18:33 +0800

    修正swiper

--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -347,28 +347,10 @@
 </style>
 <script setup>
 import MainNavBar from '../components/MainNavBarOther.vue'
-</script>
 
-<script>
 // Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from 'swiper/vue'
 // Import Swiper styles
 import 'swiper/css'
 
-import 'swiper/css/free-mode'
-import 'swiper/css/pagination'
-// import required modules
-import { FreeMode } from 'swiper/modules'
-
-export default {
-  components: {
-    Swiper,
-    SwiperSlide,
-  },
-  setup() {
-    return {
-      modules: [FreeMode],
-    }
-  },
-}
 </script>
